c2f84f34a4173960adb956b946df39c22a243b57
unknown <dkdkdk0429@naver.com>
2020-07-19 16:10:55 +0900

    component test

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Moview App 2020 
 # DK
 
-# 20/07/19 v1.0
-
 React JS Fundamentals Course 2020
--- src/App.js
+++ src/App.js
@@ -1,4 +1,53 @@
 import React from 'react';
+// App은 React.Component 속성을 상속 받음
+class App extends React.Component {
+  //Class 일 뿐이라서 함수형태로 만들어주기 위해서 render() 함수를 사용 (JSX를 반환)
+  //함수형 Component : return 문이 JSX 반환 vs 클래스형 Component : render 함수가 JSX 반환
+  constructor(props){
+    super(props);
+    console.log('hello');
+  }
+  state = {
+    count : 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count : current.count +1,
+    }));
+  };
+
+  minus = () =>{
+    this.setState(current => ({
+      count : current.count -1,
+    }));
+  };
+  conmponentDidMount(){
+    console.log('compent rendered');
+  }
+  componentDidUpdate(){
+    console.log('I just updated');
+  }
+  componentWillUnmount(){
+    console.log('GoodBye');
+  }   
+  render(){
+    console.log('rendering');
+    return(
+        <div>
+          <h1> The number is : {this.state.count}</h1>
+          <button onClick={this.add}>Add</button>
+          <button onClick={this.minus}>Minus</button>
+        </div>
+      ); 
+    }
+}
+
+export default App;
+
+
+/*
+import React from 'react';
 function Food({ name, picture }){
   return (
     <div>
@@ -44,3 +93,4 @@ function App() {
 }
 
 export default App;
+*/
