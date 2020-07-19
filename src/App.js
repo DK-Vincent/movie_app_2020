e0aa85c81ba667a6af8d1ed33171e445a61ac35f
unknown <dkdkdk0429@naver.com>
2020-07-19 11:01:11 +0900

    2020-07-19 v1.0 Food map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+function Food({ name, picture }){
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>    
+  )
+}
+
+const foodILike = [
+  {
+    name : 'Hand Made Burger',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.q8rSvHfjv9kNfP7PojeDhgHaGM%26pid%3DApi%26h%3D160&f=1',
+  },
+  {
+    name : 'Burger King',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.q8rSvHfjv9kNfP7PojeDhgHaGM%26pid%3DApi%26h%3D160&f=1',
+  },
+  {
+    name : 'Shack Shack',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.q8rSvHfjv9kNfP7PojeDhgHaGM%26pid%3DApi%26h%3D160&f=1',
+  },
+  {
+    name : 'Macdonalds',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.q8rSvHfjv9kNfP7PojeDhgHaGM%26pid%3DApi%26h%3D160&f=1',
+  },
+  {
+    name : 'Lotteria',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.q8rSvHfjv9kNfP7PojeDhgHaGM%26pid%3DApi%26h%3D160&f=1',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {foodILike.map(dish => 
+      (<Food name = {dish.name} picture = {dish.image} />
+      
+      ))}
+    
+  </div>
   );
 }
 
